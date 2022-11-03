15f75c9541ce979725eee703c7ec455931110654
w68141 <yr.hvrn15@gmail.com>
2022-11-03 11:57:06 +0100

    Nie zrobiono jeszcze przypadok jeśli x = y

---
+++ Grupa1_Zadanie1.py
@@ -0,0 +1,9 @@
+x = int(input("Podaj liczbę calkowitę nr.1: "))
+y = int(input("Podaj liczbę calkowitę nr.2: "))
+
+if x < y:
+    print("Ta liczba jest mniejsza: ", x)
+elif y < x:
+    print("Ta liczba jest mniejsza: ", y)
+else:
+    print("Danny nie są poprawny!")
